de23e96a216da2030a659f9b2d3275e5d582330a
Paulo <pauloakira72@gmail.com>
2025-05-03 00:12:14 -0300

    mexendo no codigo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     cssCodeSplit: true,
     rollupOptions: {
@@ -19,5 +25,6 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    open: true,
   },
 });
