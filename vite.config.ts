dcdc95c260e70e146944dbc5259a243459fed659
Paulo <pauloakira72@gmail.com>
2025-05-03 00:38:57 -0300

    fix: correção de estilização e assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ESG/',
   build: {
-    cssCodeSplit: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
@@ -14,12 +17,8 @@ export default defineConfig({
       },
     },
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
   server: {
     port: 3000,
     host: true,
   },
-  base: '/ESG/',
 });
